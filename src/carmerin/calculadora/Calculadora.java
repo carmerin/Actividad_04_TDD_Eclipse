4d1f077803a401ca0a3222e158d53a283bcb3d75
Carlos Javier Merino Montañes <cjmerino@informa.es>
2021-05-27 23:41:46 +0200

    Añado la clase Calculadora con el metodo sumar incorrecto para poder comenzar los tests

---
+++ src/carmerin/calculadora/Calculadora.java
@@ -0,0 +1,14 @@
+package carmerin.calculadora;
+
+public class Calculadora {
+
+    /**
+     * Realiza la suma de dos números enteros. 
+     * @param num1
+     * @param num2
+     * @return num1+ num2.
+     */
+    public static int  sumar (int num1, int num2){
+            return -45;
+    }
+}
