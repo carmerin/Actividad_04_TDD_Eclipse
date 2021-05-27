7227e65a45e8d420bd6ff31c5dc33afc90be668e
Carlos Javier Merino Montañes <cjmerino@informa.es>
2021-05-28 00:21:56 +0200

    Implemento el método dividir correcto, controlando que el divisor no sea 0 y lanzando la excepción esperada si ocurre

--- src/carmerin/calculadora/Calculadora.java
+++ src/carmerin/calculadora/Calculadora.java
@@ -43,6 +43,13 @@ public class Calculadora {
      */
     public static  double  dividir (int num1, int num2){
             
-        return num1 / num2;
+        if(num2 == 0) {
+            
+            throw new IllegalArgumentException("Division by zero is not supported");
+        }
+        else {        
+            
+            return num1 / num2;
+        }
     }
 }
